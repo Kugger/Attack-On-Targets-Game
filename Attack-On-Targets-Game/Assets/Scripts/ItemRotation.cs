3f8be7c8951c0e6d7dd276c3ed6ba219a12e64b9
Kacper Gorgoń <kacper15011@gmail.com>
2020-11-11 22:28:23 +0100

    Add animations

--- Attack-On-Targets-Game/Assets/Scripts/ItemRotation.cs
+++ Attack-On-Targets-Game/Assets/Scripts/ItemRotation.cs
@@ -6,8 +6,18 @@ public class ItemRotation : MonoBehaviour
 {
     public float x = 0, y = 0, z = 0;
 
+    public bool waving = false;
+
     void Update()
     {
-        transform.Rotate(new Vector3(x, y, z) * Time.deltaTime);
+        if (waving == true) 
+        {
+            transform.Rotate(new Vector3(x, y, z) * Time.deltaTime);
+
+            transform.position = transform.up + new Vector3(transform.position.x, Mathf.Sin(Time.time * 2f) * .5f, transform.position.z) ;
+        } else
+        {
+            transform.Rotate(new Vector3(x, y, z) * Time.deltaTime);
+        }
     }
 }
---
+++ Attack-On-Targets-Game/Assets/Scripts/SlowDownPowerUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowDownPowerUp : MonoBehaviour
+{
+    public float slowDownAmount = 0.05f;
+    public float slowDownLength = 2f;
+
+    private void Update()
+    {
+        Time.timeScale += (1f / slowDownLength) * Time.unscaledDeltaTime;
+        Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Time.timeScale = slowDownAmount;
+        Time.fixedDeltaTime = Time.timeScale * .02f;
+
+        // transform.parent.gameObject.SetActive(false);
+        // collision.gameObject.SetActive(false);
+    }
+}
