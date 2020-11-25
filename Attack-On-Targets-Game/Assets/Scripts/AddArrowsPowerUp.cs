257426c9855dc7c041ac85eab3a57ecfceab87c1
Kacper Gorgoń <kacper15011@gmail.com>
2020-11-25 19:47:33 +0100

    Add AddArrowPowerUp scripit

---
+++ Attack-On-Targets-Game/Assets/Scripts/AddArrowsPowerUp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddArrowsPowerUp : MonoBehaviour
+{
+    [SerializeField]
+    Text numberOfArrowsText; // tutaj bedziemy przypisywac nasz tekst
+
+    int numberOfArrowsInt;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject ArrowSpawnPosition = GameObject.Find("ArrowSpawnPosition"); // znajduje obiekt ArrowSpawnPosition do ktorego przypisany jest skrypt Shoot
+        Shoot shoot = ArrowSpawnPosition.GetComponent<Shoot>(); // zbiera komponenty (w tym wartosci) ze skryptu Shoot
+
+        numberOfArrowsInt = ArrowSpawnPosition.GetComponent<Shoot>().numberOfArrows;
+
+        numberOfArrowsText.text = numberOfArrowsInt.ToString(); // wyswietlamy informacje
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        numberOfArrowsInt += 2;
+
+        numberOfArrowsText.text = numberOfArrowsInt.ToString();
+
+        transform.parent.gameObject.SetActive(false);
+        collision.gameObject.SetActive(false);
+    }
+}
