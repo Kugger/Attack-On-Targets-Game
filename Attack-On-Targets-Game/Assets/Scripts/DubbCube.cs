3233d76e83f2a13724b17cfe53061515496e01e8
Kacper Gorgoń <kacper15011@gmail.com>
2021-02-04 18:42:57 +0100

    Added assets to the TutorialScene, music and teleport between levels

---
+++ Attack-On-Targets-Game/Assets/Scripts/DubbCube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DubbCube : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource dubbSource;
+
+    public GameObject scriptPanel;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        dubbSource.Play();
+
+        scriptPanel.SetActive(true);
+    }
+}
---
+++ Attack-On-Targets-Game/Assets/Scripts/Teleport.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleport : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Scene");
+        SceneManager.LoadScene("Tut2");
+    }
+}
